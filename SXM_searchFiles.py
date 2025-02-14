5e84519f70e617a5facbd86af4be4e4efd309e58
Michele <poomerang@autistici.org>
2025-02-14 16:57:14 +0100

    Removed package quantities, added Linux/Windows path compatibility

--- SXM_searchFiles.py
+++ SXM_searchFiles.py
@@ -1,11 +1,24 @@
 # coding=utf-8
-# The first 3 lines are only needed to run in python as standalone script  
-import sys
-sys.path.append("C:/Program Files (x86)/Gwyddion/bin")
-import gwy
-import glob
-import quantities as pq # see notes on quantities package in python2
+
+#################
+### Libraries ###
+#################
+
+# note that sys and gwy are only needed to run as standalone python script  
+#import sys
+### detect OS (so \ and / in paths are changed automatically) and set path for gwy library
 import os
+if os.name == 'nt':
+  sys.path.append("C:/Program Files (x86)/Gwyddion/bin")
+  windows = 1
+  linux = 0
+elif os.name == 'posix':
+  sys.path.append("/var/lib/flatpak/app/net.gwyddion.Gwyddion/current/active/files/lib/python2.7/site-packages")
+  linux = 1
+  windows = 0
+#import gwy
+
+import glob
 import shutil
 
 
@@ -13,12 +26,11 @@ import shutil
 ###### SEARCH OPTIONS ######
 ############################
 
-
 ### Folder and filetype ###
 # Set folder(s) to search (use */* for recursive searches) 
 # and filetype (uncomment as needed)
-folder = r'\\synology13.jh-inst.cas.cz\NCG\Michele\DATA\STM\2024-10 P127 Valerie\2024-10-22 P127 sputter 5s'
-#filetype = '.gwy' 
+folder = r'D:\DATA\STM\*\*'
+filetype = '.gwy' 
 filetype = '.sxm' 
 #filetype = 'both'
 
@@ -28,19 +40,18 @@ filetype = '.sxm'
 # uncomment target_folder and set accordingly
 if 'target_folder' in locals():
   del target_folder
-#target_folder = r'D:\DATA\STM\2024-10 P127 Valerie\2024-10-09 P127 scan2\defects_30nm'
+#target_folder = r'D:\DATA\STM\Defects MoS2 - Valerie\2024-10 P127 Valerie\bubbles'
 
 
 ### Search filters ###
 
-# Filter on the filename (insert between ' ')
+# Filter on the filename (between ' '; for syntax and wildcards see https://pymotw.com/2/glob/)
 filter_filename = '*' 
 
 # Filter on values
-# options: size, bias, speed, setpoint, scantime (in seconds),
-#  scantime_min (only minutes)
 
 # Field 1, >=
+# options: size, bias, speed, setpoint, scantime (in seconds), scantime_min (only minutes)
 filter_field1 = 'size' # name of the field to apply >= filter 
 filter_value1 = 0 # value, pass >=
 # Field 2, <=
@@ -58,9 +69,9 @@ filter_comment2 = '' # filter for comment line #2
 # Full filename or filenr. only.  0: full, 1: only image number
 filename_or_filenr = 0
 # Bias and setpoint.  0: no, 1: yes
-print_scanparams = 1
+print_scanparams = 0
 # Size and speed.  0: nothing, 1: image size, 2: image size + speed and direction
-print_size_speed = 1
+print_size_speed = 0
 # Print the scan time. 0: no, 1: yes
 print_time = 0 
 
@@ -73,15 +84,37 @@ print_time = 0
 #print 'Starting script'
 counter = 0
 
+### search file by name and extension
 if filetype == '.gwy' or filetype == '.sxm':
-  filelist = glob.glob(folder + '\\' + filter_filename + filetype)
+  if windows:
+    filelist = glob.glob(folder + '\\' + filter_filename + filetype) # Windows
+  elif linux:
+    filelist = glob.glob(folder + '/' + filter_filename + filetype) # Unix
 elif filetype == 'both':
-  filelist = glob.glob(folder + '\\' + filter_filename + '.sxm') + glob.glob(folder + '\\' + filter_filename + '.gwy')
+  if windows:
+    filelist = glob.glob(folder + '\\' + filter_filename + '.sxm') + glob.glob(folder + '\\' + filter_filename + '.gwy') # Windows
+  elif linux:
+    filelist = glob.glob(folder + '/' + filter_filename + '.sxm') + glob.glob(folder + '/' + filter_filename + '.gwy')
 else:
   print 'Filetype not recognized'
+
+# clean duplicates (useless if file extension is included..)
+# filelist_unique = set(filelist)
   
 if len(filelist) != 0:
   for stmfile in filelist:
+  
+    ## Get filename/number
+    # split the full path into a list
+    # last element of the list is the filename
+    # the number is the last two digits (before . and extension)
+    if windows:
+      path_as_list = stmfile.split("\\")
+    elif linux:
+      path_as_list = stmfile.split("/")
+    filename_ext = path_as_list[-1]
+    
+    #print filename_ext
     
     ## Get current file
     container = gwy.gwy_file_load(stmfile, gwy.RUN_INTERACTIVE)
@@ -95,14 +128,16 @@ if len(filelist) != 0:
     feedback = metadata.get_string_by_name('Z-Controller::Controller name')
     setpoint = float(metadata.get_string_by_name('Z-Controller::Setpoint'))
     setpoint_unit = metadata.get_string_by_name('Z-Controller::Setpoint unit')
-    bias = float(metadata.get_string_by_name('Bias::Bias (V)'))*pq.V
+    bias = float(metadata.get_string_by_name('Bias::Bias (V)')) # in V 
     osc =  metadata.get_string_by_name('Oscillation Control::output off')
-    osc_ampli = metadata.get_double_by_name('Oscillation Control::Amplitude Setpoint (m)') * pq.m
-    speed_m = float(metadata.get_string_by_name('Scan::speed forw. (m/s)'))*pq.m/pq.s
-    speed = round(speed_m.rescale(pq.nm/pq.s),0)*pq.nm/pq.s
+    osc_ampli_m = metadata.get_double_by_name('Oscillation Control::Amplitude Setpoint (m)') # in m
+    osc_ampli = osc_ampli_m*(1000000000000) # in pm 
+    speed_m = float(metadata.get_string_by_name('Scan::speed forw. (m/s)')) # in m/s
+    #speed = round(speed_m.rescale(pq.nm/pq.s),0)*pq.nm/pq.s
+    speed = round(speed_m*1000000000,0) # in nm/s
     direction = metadata.get_string_by_name('Direction')
     scantime_str = metadata.get_string_by_name('Acquistion time')
-    scantime = float(scantime_str.replace(',', '.').split()[0])#*pq.s
+    scantime = float(scantime_str.replace(',', '.').split()[0]) # in seconds
     scantime_min = int(scantime//60)
     scantime_s = int(scantime%60)
     time = metadata.get_string_by_name('Time')
@@ -112,24 +147,24 @@ if len(filelist) != 0:
 
     ## Make the setpoint string according to mode (STM, AFM or combined)
     if controller_status == 'OFF': # constant height
-        height = metadata.get_double_by_name('Z-Controller::TipLift (m)')*pq.m
+        height = metadata.get_double_by_name('Z-Controller::TipLift (m)') * 10000000000 # in Å
         if osc == 'TRUE':
-            mode = 'CH STM+osc %i' % osc_ampli.rescale(pq.pm) + 'pm, dz= %.1f' % height.rescale(pq.angstrom) + 'Ã…'
+            mode = 'CH STM+osc %i' % osc_ampli + 'pm, dz= %.1f' % height + 'Ã…'
         elif osc == 'FALSE':
-            mode = 'CH, dz= %.1f' % height.rescale(pq.angstrom) + 'Ã…'
+            mode = 'CH, dz= %.1f' % height + 'Ã…'
     elif controller_status == 'ON': # constant current/df
         #mode = 'CC'
         if feedback.startswith("log"): #STM
             if osc == 'TRUE':
-                mode = 'STM+osc %i' % osc_ampli.rescale(pq.pm) + 'pm'
+                mode = 'STM+osc %i' % osc_ampli + 'pm'
             elif osc == 'FALSE':
                 mode = ''
-            setpt = setpoint * pq.A
-            setpt_txt = ' I=%.2f' % setpt.rescale(pq.nA) + ' nA'
+            setpt = setpoint*1000000000 # in nA
+            setpt_txt = ' I=%.2f' % setpt + ' nA'
         elif feedback.startswith("FM"): #AFM
-            mode = 'AFM osc %i' % osc_ampli.rescale(pq.pm) + 'pm'
-            setpt = setpoint * pq.Hz
-            setpt_txt = ' df=%.2f' % setpt + ' Hz'
+            mode = 'AFM osc %i' % osc_ampli + 'pm'
+            #setpt = setpoint * pq.Hz
+            setpt_txt = ' df=%.2f' % setpoint + ' Hz'
 
 
     ### GET IMAGE PROPERTIES ###
@@ -140,10 +175,10 @@ if len(filelist) != 0:
     # create the DataField with the image
     data_field = container[gwy.gwy_app_get_data_key_for_id(ids[0])]
     # get x,y scan size
-    sizeX_m = data_field.get_xreal()*pq.m # in m
-    sizeX = round(sizeX_m.rescale(pq.nm)) # in nm, float
-    sizeY_m = data_field.get_yreal()*pq.m 
-    sizeY = round(sizeY_m.rescale(pq.nm)) 
+    #sizeX_m =  # in m
+    sizeX = round(data_field.get_xreal()*1000000000) # in nm, float
+    #sizeY_m = *pq.m 
+    sizeY = round(data_field.get_yreal()*1000000000) # in nm 
     size = sizeX
 
 
@@ -153,13 +188,6 @@ if len(filelist) != 0:
     if locals()[filter_field1] >= filter_value1 and locals()[filter_field2] <= filter_value2 and comment.find(filter_comment1) != -1 and comment.find(filter_comment2) != -1:
      
      counter += 1
-     
-     ## Get filename/number
-     # split the full path into a list
-     # last element of the list is the filename
-     # the number is the last two digits (before . and extension)
-     path_as_list = stmfile.split("\\")
-     filename_ext = path_as_list[-1]
      
 
      ## Copy files if target_folder exists
@@ -176,9 +204,12 @@ if len(filelist) != 0:
 
      # build "file number/name: comment" and justification params
      if filename_or_filenr == 0: # if using filename
-        filename = filename_ext[0:-4]
+        filename = filename_ext#[0:-4]
         longest = max(filelist, key = len) # longest filename with path; filelist.index() to find its index
-        length = len(longest.split('\\')[-1][0:-4])
+        if windows:
+          length = len(longest.split('\\')[-1][0:-4])
+        elif linux:
+          length = len(longest.split('/')[-1][0:-4])
         justify = [length, length+2]
         intestation = filename.ljust(justify[0]), ':', comment 
      elif filename_or_filenr == 1: # if using filenr
