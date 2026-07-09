0aa4dce2533826f57b2c9e57bd8dfc6e7b377d3f
Michele <emmegamma@inventati.org>
2026-07-09 17:43:14 +0200

    run interactively for 1st image
    At the 1st image of each batch, the Save Image dialog appears, to allow choosing settings (scale bar, font, etc.) for the whole batch rather than applying the last used ones

--- batch_export_to_image.py
+++ batch_export_to_image.py
@@ -8,31 +8,31 @@
 # import sys  
 import glob, shutil
 import os, itertools
-### detect OS so that \ and / are set automatically in paths + set path for gwy library
+### set path for gwy library and set \ or / in paths
 if os.name == 'nt':
-  sys.path.append("C:/Program Files (x86)/Gwyddion/bin")
   windows = 1
+  sys.path.append("C:/Program Files (x86)/Gwyddion/bin")
+  split_char = "\\"
 elif os.name == 'posix':
-  sys.path.append("/var/lib/flatpak/app/net.gwyddion.Gwyddion/current/active/files/lib/python2.7/site-packages")
   windows = 0
+  sys.path.append("/var/lib/flatpak/app/net.gwyddion.Gwyddion/current/active/files/lib/python2.7/site-packages")
+  split_char = "/"
 #import gwy
 
 ############################
 ######## OPTIONS ###########
 ############################
 
-### Folder and filetype ###
-# Set folder(s) to search (use */* for recursive searches) 
-# and filetype (uncomment as needed)
-folder = r'D:\DATA\STM'
-filetype = '.gwy' 
+# Set folder(s) and filetype to search (use */* for recursive searches) 
+folder = r'C:\Users\STM\Documents\DATA\atomic res\P148'
+#filetype = '.gwy' 
 #filetype = '.sxm'
-#filetype = 'both'
+filetype = 'both'
 
 # Filter on the filename (keep between ' '; for syntax and wildcards see https://pymotw.com/2/glob/)
 filter_filename = '*' 
 
-# OVERWRITE: set to 1 to allow overwrite, otherwise existing files will be skipped
+# OVERWRITE: set to 1 to allow overwrite, 0 to skip existing files
 overwrite = 1
 
 # DRY RUN: set to 1 to test without actually writing files
@@ -42,12 +42,11 @@ dry_run = 0
 ####### SCRIPT START #######
 ############################
 
-# Change the current folder's directory and get the name
+# Change the working directory (os.getcwd() to check)
 os.chdir(folder)
-location = os.getcwd()
+print 'Working dir: ' + os.getcwd()
 
 ### search file by name and extension
-separator = '\\' if windows==1 else '/'
 if filetype == '.gwy' or filetype == '.sxm':
     filelist = glob.glob(filter_filename + filetype) 
 elif filetype == 'both':
@@ -56,7 +55,7 @@ else:
   print 'Filetype not recognized'
   
 # create list for not-overwritten files
-existing_files = []
+not_overwritten = []
  
 
 if len(filelist) != 0:
@@ -64,49 +63,49 @@ if len(filelist) != 0:
   for stmfile in filelist:
   
     ## Get filename/number
-    # split the full path into a list
-    # last element of the list is the filename
-    # the number is the last two digits (before . and extension)
-    if windows:
-      path_as_list = stmfile.split("\\")
-    elif linux:
-      path_as_list = stmfile.split("/")
+    # split the full path into a list, the last element is the filename
+    path_as_list = stmfile.split(split_char)
     filename = path_as_list[-1]
     filebase = filename[0:-4]
     imagefile = filebase + '.jpg'
-    print_text = 'Writing ' 
     
-    if os.path.exists(imagefile):
-         if overwrite != 1: #overwrite will work only if it's exactly 1
-             existing_files.append(imagefile)    
-    	     continue
-    	 else:
-    	     print_text = 'Overwriting '
+    if os.path.exists(imagefile) == False:
+      output_text = 'Writing '
+    else:
+      if overwrite == 1:
+        output_text = 'Overwriting '
+      else:
+    	not_overwritten.append(imagefile)    
+    	continue
+      
     
     ## Get current file and add them to the data browser
     container = gwy.gwy_file_load(stmfile, gwy.RUN_INTERACTIVE)
     gwy.gwy_app_data_browser_add(container)
     
-    # save first channel of file
+    # if not in dry run, save first channel to image
     if dry_run == 0:
-        gwy.gwy_app_data_browser_select_data_field(container, 0)
+      gwy.gwy_app_data_browser_select_data_field(container, 0)
+      if stmfile==filelist[0]: #at first iteration, run interactive to set image params
+        gwy.gwy_file_save(container, imagefile, RUN_INTERACTIVE)
+      else:
         gwy.gwy_file_save(container, imagefile, RUN_NONINTERACTIVE)
     
-    # Remove the file (container) from the data browser to avoid overloading the memory
+    # Remove the container from the data browser
     gwy.gwy_app_data_browser_remove(container)
    
     # Print operation
-    print print_text + location + separator + imagefile
+    print output_text + imagefile # + folder + split_char
     
 else: ### if no files were found (len(filelist)== 0)  
   print 'No suitable files found in {}, check folder/filters'.format(folder)
 
-# if files were NOT overwritten, list them    
-if len(existing_files) != 0:
-    print '\nOverwrite was 0, so the following files were skipped (existed already):'
-    for i in existing_files:
-        print i
+# list NOT overwritten files
+if len(not_overwritten) != 0:
+    print '\nFiles skipped (overwrite = 0):'
+    for i in not_overwritten:
+        print ' {}'.format(i)
 
 # if dry_run, say it
 if dry_run == 1:
-    print '(Dry run, no files actually written)'
+    print '\r(Dry run, no files were actually written)'
